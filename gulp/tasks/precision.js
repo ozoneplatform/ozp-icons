d943ee0d000dee823db18923712d7f5a7f5b3a82
clarkjefcoat <clark.j.jefcoat@ugov.gov>
2016-04-26 17:27:48 -0400

    fix(precision): Correct precision so that 'px' isn't accidently truncated to 'x'

--- gulp/tasks/precision.js
+++ gulp/tasks/precision.js
@@ -8,39 +8,20 @@ var post    = require('gulp-postcss');
 
 // Define the plugin.
 var precision = postcss.plugin('postcss-precision', function() {
-  var longEmTest = /(\d+?\.\d{3,})(%|em|px)/gi;
+  var longEmTest = /(\d+?\.\d{3,})(?=(%|em|px))/gi;
 
   return function(style) {
     style.walkDecls(function(decl) {
-
 
       if (! decl.value || longEmTest.test(decl.value)) {
         // Grab array of matches.
         var matches = (decl.value + '').match(longEmTest);
         // We'll assume there's one
-        if(matches.length === 1){
-        var value = matches[0].substr(0, matches[0].length - 1);
-        // Round two decimal places.
-        var rounded = Math.round(parseFloat(value) * 100) / 100;
-        // Change the value in the tree.
-        decl.value = decl.value.replace(value, rounded.toString());
-      }
-      else{
-        for(var i = 0; i < matches.length-1; i++) {
-          var value = matches[i].substr(0, matches[i].length - 1);
+        for(var i = 0; i < matches.length; i++) {
+          var value = matches[i];
           var rounded = Math.round(parseFloat(value) * 100) / 100;
           decl.value = decl.value.replace(value, rounded.toString());
-          for(var j = i; j < matches.length; j++) {
-              var value = matches[j].substr(0, matches[j].length - 1);
-              // Round two decimal places.
-              var rounded = Math.round(parseFloat(value) * 100) / 100;
-              // Change the value in the tree.
-              decl.value = decl.value.replace(value, rounded.toString());
-          }
         }
-      }
-
-
       }
     });
   };
